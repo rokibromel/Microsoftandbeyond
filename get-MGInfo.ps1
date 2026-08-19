9cac64945a3d8d37f5725a40b30b3f90a428848e
rokibromel <rokib.romel@gmail.com>
2026-08-19 12:12:48 +0600

    Add files via upload

---
+++ get-MGInfo.ps1
@@ -0,0 +1,15 @@
+Connect-MgGraph -Scopes "User.Read.All"
+
+$Details = Get-MgContext
+$Scopes = $Details | Select-Object -ExpandProperty Scopes
+$Scopes = $Scopes -join ","
+$OrgName = (Get-MgOrganization).DisplayName
+
+"Microsoft Graph current session details:"
+"---------------------------------------"
+"Tenant Id = $($Details.TenantId)"
+"Client Id = $($Details.ClientId)"
+"Org name  = $OrgName"
+"App Name  = $($Details.AppName)"
+"Account   = $($Details.Account)"
+"Scopes    = $Scopes"
